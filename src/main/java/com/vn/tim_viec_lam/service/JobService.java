46be68785b922fff249155747a289bb9855010c4
VanDuc263 <22130050@st.hcmuaf.edu.vn>
2024-12-16 18:01:34 +0700

    code method get job form database for JobDao

--- src/main/java/com/vn/tim_viec_lam/service/JobService.java
+++ src/main/java/com/vn/tim_viec_lam/service/JobService.java
@@ -1,4 +1,19 @@
 package com.vn.tim_viec_lam.service;
 
+import com.vn.tim_viec_lam.dao.JobDao;
+import com.vn.tim_viec_lam.dao.model.Job;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JobService {
+    JobDao jobDao = new JobDao();
+    public JobService() {}
+
+    public List<Job> getAllJob(){
+        return jobDao.getAll();
+    }
+    public List<Job> getNewJob(){
+        return jobDao.get4NewJob();
+    }
 }
