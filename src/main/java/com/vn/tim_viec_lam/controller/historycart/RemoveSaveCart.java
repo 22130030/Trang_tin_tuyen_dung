2bf31937af10556f71bc0193cedbb150a92cce15
VanDuc263 <22130050@st.hcmuaf.edu.vn>
2024-12-19 14:29:06 +0700

    code history viewed for home_account.jsp

--- src/main/java/com/vn/tim_viec_lam/controller/historycart/RemoveSaveCart.java
+++ src/main/java/com/vn/tim_viec_lam/controller/historycart/RemoveSaveCart.java
@@ -2,8 +2,10 @@ package com.vn.tim_viec_lam.controller.historycart;
 
 import com.vn.tim_viec_lam.dao.model.Job;
 import com.vn.tim_viec_lam.dao.model.cart.HistoryCart;
+import com.vn.tim_viec_lam.dao.model.cart.SavingCart;
 import com.vn.tim_viec_lam.service.JobService;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,26 +13,17 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-public class AddSaveCart extends HttpServlet {
+@WebServlet(name = "removeSaveCart",value = "/remove-saveCart")
+public class RemoveSaveCart extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
 
-        if(request.getParameter("jid") != null) {
-
-            int id = Integer.parseInt(request.getParameter("jid"));
-            JobService js = new JobService();
-
-            Job job = js.getJobById(id);
-
-            HttpSession session = request.getSession();
-
-            HistoryCart historyCart = (HistoryCart) session.getAttribute("hsCart");
-            if(historyCart == null) {
-                historyCart = new HistoryCart();
-            }
-            historyCart.addJobCart(job);
-            session.setAttribute("hsCart", historyCart);
+        HttpSession session = request.getSession();
+        HistoryCart sc = (HistoryCart) session.getAttribute("hsCart");
+        if(sc != null){
+            session.removeAttribute("hsCart");
         }
+        response.sendRedirect(request.getContextPath() + "/home_account.jsp");
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
