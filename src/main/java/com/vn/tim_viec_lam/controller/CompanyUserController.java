deffab8b84539c2fdd45de044c3f8053b29fca7f
22130030 <22130030@st.hcmuaf.edu.vn>
2025-01-07 10:01:59 +0700

    Temporary commit to save changes

--- src/main/java/com/vn/tim_viec_lam/controller/CompanyUserController.java
+++ src/main/java/com/vn/tim_viec_lam/controller/CompanyUserController.java
@@ -1,4 +1,22 @@
 package com.vn.tim_viec_lam.controller;
 
-public class CompanyUserController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet(name = "company-user-controller",value = "/company-user-controller")
+public class CompanyUserController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
 }
