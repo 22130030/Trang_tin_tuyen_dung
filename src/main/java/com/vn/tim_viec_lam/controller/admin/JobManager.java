dabe7894850e9ca98f30d7ccc7e5ed6a643e17bd
22130030 <22130030@st.hcmuaf.edu.vn>
2025-01-12 23:10:18 +0700

    Add list job in admin

--- src/main/java/com/vn/tim_viec_lam/controller/admin/JobManager.java
+++ src/main/java/com/vn/tim_viec_lam/controller/admin/JobManager.java
@@ -1,4 +1,29 @@
 package com.vn.tim_viec_lam.controller.admin;
 
-public class JobManager {
+import com.vn.tim_viec_lam.dao.model.Job;
+import com.vn.tim_viec_lam.service.JobService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "job_manager",value = "/job_manager")
+public class JobManager extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        JobService service = new JobService();
+        List<Job>listJob = service.getAllJob();
+        req.setAttribute("list", listJob);
+        req.getRequestDispatcher("admin_jobs.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
