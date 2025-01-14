0d4738ddf4c71a9a67d14798f31241a981c5f14b
22130030 <22130030@st.hcmuaf.edu.vn>
2025-01-15 02:20:26 +0700

    Complete function edit job category name in admin

--- src/main/java/com/vn/tim_viec_lam/service/JobCategoryService.java
+++ src/main/java/com/vn/tim_viec_lam/service/JobCategoryService.java
@@ -17,7 +17,13 @@ public class JobCategoryService {
     public List<JobCategory> FindListCategoryByNameJob(String jobName) {
         return jobCategoryDao.FindListJobCategroy(jobName);
     }
+    public JobCategory FindListCategoryNameJobByID(int id) {
+        return jobCategoryDao.FindListJobCategroyByID(id);
+    }
     public void deleteCategory(int id) {
         jobCategoryDao.deleteJobPostCategory(id);
+    }
+    public void editCategory(int id,String jobCategory,String jobCategoryName) {
+        jobCategoryDao.editCategory(id, jobCategory, jobCategoryName);
     }
 }
