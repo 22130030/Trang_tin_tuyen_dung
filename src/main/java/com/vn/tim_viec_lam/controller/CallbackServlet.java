c893645c0e307ae3ae8b3592d9da451d533767c1
22130030 <22130030@st.hcmuaf.edu.vn>
2025-03-23 12:15:58 +0700

    Complete function login in google

---
+++ src/main/java/com/vn/tim_viec_lam/controller/CallbackServlet.java
@@ -0,0 +1,66 @@
+package com.vn.tim_viec_lam.controller;
+
+import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@WebServlet(name="callback",value = "/callback")
+public class CallbackServlet extends HttpServlet {
+    private static final String CLIENT_ID = "m";
+    private static final String CLIENT_SECRET = "m";
+    private static final String REDIRECT_URI = "m";
+
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final NetHttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("code");
+
+        if (code == null) {
+            resp.sendRedirect("login");
+            return;
+        }
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, CLIENT_ID, CLIENT_SECRET,
+                Collections.singletonList("https://www.googleapis.com/auth/userinfo.profile"))
+                .build();
+
+        TokenResponse tokenResponse = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
+        String accessToken = tokenResponse.getAccessToken();
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(HTTP_TRANSPORT, JSON_FACTORY)
+                .setAudience(Collections.singletonList(CLIENT_ID))
+                .build();
+
+        String idTokenString = tokenResponse.get("id_token").toString();
+        GoogleIdToken idToken = GoogleIdToken.parse(JSON_FACTORY, idTokenString);
+
+
+        if (idToken != null) {
+            GoogleIdToken.Payload payload = idToken.getPayload();
+            String userId = payload.getSubject();
+            String email = payload.getEmail();
+            String name = (String) payload.get("name");
+
+            req.getSession().setAttribute("user", name);
+            req.getSession().setAttribute("email", email);
+            resp.sendRedirect("home");
+        } else {
+            resp.sendRedirect("login");
+        }
+    }
+}
